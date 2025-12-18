784f1212abe6e8721164f81b7f8ac78ea9a4a842
Iyad <iyad.mahdy@hotmail.com>
2025-12-18 21:03:42 +0200

    Fix flake8 error

--- src/data/clean_numeric.py
+++ src/data/clean_numeric.py
@@ -27,7 +27,7 @@ def clean_numeric_features(
     # Convert TotalCharges to numeric
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
 
-    # Impute missing TotalCharges with 0 since missing values correspond to customers with no tenure
+    # Impute with 0 since missing values correspond to customers with no tenure
     df["TotalCharges"] = df["TotalCharges"].fillna(0)
 
     # Save cleaned dataset
